b019f16be9eed70497467d9aafece76728b276a0
Taweesak Jantasan <em.94xd@outlook.com>
2025-12-11 16:42:23 +0700

    change to astro framework

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://em94xd-png.github.io',
+    base: 'pj_webpage'
+});
